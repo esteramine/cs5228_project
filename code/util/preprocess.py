d12d4b1293d7eaae746f1c1935d7e007136140e3
ZeeTsing <51752727+ZeeTsing@users.noreply.github.com>
2025-10-09 21:33:32 +0800

    add model and flat type process

--- code/util/preprocess.py
+++ code/util/preprocess.py
@@ -5,6 +5,28 @@ def get_floor_avg(floor_range):
     floors = [int(x) for x in floor_range.split(' to ')]
     return int((floors[0] + floors[1]) / 2)
 
+def update_flat_type(flat):
+    if flat == "2 ROOM" or flat == "1 ROOM":
+        return "1 / 2 ROOM"
+    elif flat == "EXECUTIVE" or flat == 'MULTI-GENERATION':
+        return 'EXECUTIVE & MULTI-GENERATION'
+    else: 
+        return flat
+
+def update_model_type(flat):
+    if flat == 'Model A-Maisonette' or flat == 'Improved-Maisonette' or flat == 'Premium Maisonette':
+        return 'Maisonette'
+    elif flat == 'Premium Apartment' or flat == 'Premium Apartment Loft':
+        return 'Apartment'
+    elif flat == 'Terrace' or flat == 'Multi Generation' or flat =='Adjoined flat':
+        return "Terrace/Multi-Gen/Adjoined"
+    elif flat == 'Model A2':
+        return "Model A"
+    elif flat == 'Type S1' or flat == 'Type S2':
+        return "Type S"
+    else: 
+        return flat
+
 
 def preprocess(df_original):
     df = df_original.copy()
